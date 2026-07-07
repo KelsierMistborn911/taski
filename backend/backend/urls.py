e36cc83e2e2a75facb05487a1a3c3753ecf3e356
Никита Маркиянов <ckim911@yandex.ru>
2026-07-07 21:48:25 +0300

    Add root view for Taski

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
 from rest_framework import routers
-
 from api import views
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
+    path('', lambda request: HttpResponse('<html><body>Taski</body></html>')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]

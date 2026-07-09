ba973356c5e6a08f1ab67fdcf8fd908122f00d45
Никита Маркиянов <ckim911@yandex.ru>
2026-07-10 01:04:07 +0300

    Add static js route

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.http import HttpResponse
+from django.views.static import serve
 from rest_framework import routers
 from api import views
+import os
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
@@ -11,4 +13,5 @@ urlpatterns = [
     path('', lambda request: HttpResponse('<html><body>Taski<script src="/static/js/main.js"></script></body></html>')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('static/js/main.js', lambda request: HttpResponse('// Taski', content_type='application/javascript')),
 ]
